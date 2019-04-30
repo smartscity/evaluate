e25fb335032fcd7ec083ef6558161202efc6aea0
chenqianwei <cqw_email@163.com>
2019-05-01 00:57:50 +0800

    专家评审页面接口联调

--- src/main/webapp/app/entities/evaluationExample/example.service.ts
+++ src/main/webapp/app/entities/evaluationExample/example.service.ts
@@ -1,35 +1,44 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import * as moment from 'moment';
+import { DATE_FORMAT } from 'app/shared/constants/input.constants';
+import { map } from 'rxjs/operators';
 
 import { SERVER_API_URL } from 'app/app.constants';
 import { createRequestOption } from 'app/shared';
-import { ISpeaker } from 'app/shared/model/speaker.model';
+import { IEvaluation } from 'app/shared/model/evaluation.model';
 
-type EntityResponseType = HttpResponse<ISpeaker>;
-type EntityArrayResponseType = HttpResponse<ISpeaker[]>;
+type EntityResponseType = HttpResponse<IEvaluation>;
+type EntityArrayResponseType = HttpResponse<IEvaluation[]>;
 
 @Injectable({ providedIn: 'root' })
-export class SpeakerService {
-  public resourceUrl = SERVER_API_URL + 'api/speakers';
+export class EvaluationExampleService {
+  public resourceUrl = SERVER_API_URL + 'api/evaluations';
 
   constructor(protected http: HttpClient) {}
 
-  create(speaker: ISpeaker): Observable<EntityResponseType> {
-    return this.http.post<ISpeaker>(this.resourceUrl, speaker, { observe: 'response' });
+  create(evaluation: IEvaluation): Observable<EntityResponseType> {
+    const copy = evaluation;
+    return this.http
+      .post<IEvaluation>(this.resourceUrl, copy, { observe: 'response' })
   }
 
-  update(speaker: ISpeaker): Observable<EntityResponseType> {
-    return this.http.put<ISpeaker>(this.resourceUrl, speaker, { observe: 'response' });
+  update(evaluation: IEvaluation): Observable<EntityResponseType> {
+    const copy = evaluation;
+    return this.http
+      .put<IEvaluation>(this.resourceUrl, copy, { observe: 'response' });
   }
 
   find(id: number): Observable<EntityResponseType> {
-    return this.http.get<ISpeaker>(`${this.resourceUrl}/${id}`, { observe: 'response' });
+    return this.http
+      .get<IEvaluation>(`${this.resourceUrl}/${id}`, { observe: 'response' });
   }
 
   query(req?: any): Observable<EntityArrayResponseType> {
     const options = createRequestOption(req);
-    return this.http.get<ISpeaker[]>(this.resourceUrl, { params: options, observe: 'response' });
+    return this.http
+      .get<IEvaluation[]>(this.resourceUrl, { params: options, observe: 'response' });
   }
 
   delete(id: number): Observable<HttpResponse<any>> {
