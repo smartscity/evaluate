9c6b678a699eaa8cc2f2ec702c3f76b2b4aaaaf7
chenqianwei <cqw_email@163.com>
2019-05-01 07:41:58 +0800

    专家评审初始化数据

--- src/main/webapp/app/entities/evaluationExample/example.component.ts
+++ src/main/webapp/app/entities/evaluationExample/example.component.ts
@@ -65,28 +65,66 @@ export class ExampleComponent implements OnInit, OnDestroy {
         (res: IEvaluation[]) => {
           this.evaluations = res;
           this.aboutTips = [];
+          const typeMap = {
+            taskSource: {
+              NATIONAL_PLAN: { min: 4, max: 5, tip: '分值应在4-5分之间' },
+              BUWEI_PLAN: { min: 2, max: 3, tip: '分值应在2-3分之间' },
+              OTHER_ORG: { min: 1, max: 1, tip: '分值应为1分' },
+              null:{ min: 0, max: 0, tip: '请选择任务来源' }
+            },
+            discoveryAndInnovation: {
+              HIGH: { min: 21, max: 30, tip: '分值应在21-30分之间' },
+              MIDDLE: { min: 11, max: 20, tip: '分值应在11-20分之间' },
+              GENERAL: { min: 0, max: 10, tip: '分值应在10分以下' },
+              null:{ min: 0, max: 0, tip: '请选择主要发现和创新点' }
+            },
+            advancedLevel: {
+              LEADING: { min: 21, max: 30, tip: '分值应在21-30分之间' },
+              ADVANCED: { min: 11, max: 20, tip: '分值应在11-20分之间' },
+              GENERAL: { min: 0, max: 10, tip: '分值应在10分以下' },
+              null:{ min: 0, max: 0, tip: '请选择先进程度' }
+            },
+            applicationAndPromotion: {
+              EXCELLENCE: { min: 11, max: 15, tip: '分值应在11-15分之间' },
+              OBVIOUS: { min: 6, max: 10, tip: '分值应在6-10分之间' },
+              GENERAL: { min: 0, max: 5, tip: '分值应在5分以下' },
+              null:{ min: 0, max: 0, tip: '请选择应用与推广及其效益' }
+            }
+          };
           for (let j = 0; j < this.evaluations.length; j++) {
+            let taskSourceTip =typeMap.taskSource[this.evaluations[j].taskSource].tip,
+            taskSourceMin =typeMap.taskSource[this.evaluations[j].taskSource].min,
+            taskSourceMax =typeMap.taskSource[this.evaluations[j].taskSource].max,
+              discoveryTip =typeMap.discoveryAndInnovation[this.evaluations[j].discoveryAndInnovation].tip,
+              discoveryMin =typeMap.discoveryAndInnovation[this.evaluations[j].discoveryAndInnovation].min,
+              discoveryMax =typeMap.discoveryAndInnovation[this.evaluations[j].discoveryAndInnovation].max,
+              advancedLevelTip =typeMap.advancedLevel[this.evaluations[j].advancedLevel].tip,
+              advancedLevelMin =typeMap.advancedLevel[this.evaluations[j].advancedLevel].min,
+              advancedLevelMax =typeMap.advancedLevel[this.evaluations[j].advancedLevel].max,
+              applicationTip =typeMap.applicationAndPromotion[this.evaluations[j].applicationAndPromotion].tip,
+              applicationMin =typeMap.applicationAndPromotion[this.evaluations[j].applicationAndPromotion].min,
+              applicationMax =typeMap.applicationAndPromotion[this.evaluations[j].applicationAndPromotion].max;
             const tempObj = {
               id: j,
               taskSource: {
-                tip: '请选择任务来源',
-                min: 0,
-                max: 0
+                tip: taskSourceTip,
+                min: taskSourceMin,
+                max: taskSourceMax
               },
               discoveryAndInnovation: {
-                tip: '请选择主要发现和创新点',
-                min: 0,
-                max: 0
+                tip: discoveryTip,
+                min: discoveryMin,
+                max: discoveryMax
               },
               advancedLevel: {
-                tip: '请选择先进程度',
-                min: 0,
-                max: 0
+                tip: advancedLevelTip,
+                min: advancedLevelMin,
+                max: advancedLevelMax
               },
               applicationAndPromotion: {
-                tip: '请选择应用与推广及其效益',
-                min: 0,
-                max: 0
+                tip: applicationTip,
+                min: applicationMin,
+                max: applicationMax
               },
               paper: {
                 tip: '分值应在5-10分',
@@ -124,8 +162,7 @@ export class ExampleComponent implements OnInit, OnDestroy {
   registerChangeInEvaluationsExample() {
     this.eventSubscriber = this.eventManager.subscribe('evaluationListModification', response => this.loadAll());
   }
-
-  changeTip(type, val, index) {
+  getTip(type, val, index){
     const typeMap = {
       taskSource: {
         NATIONAL_PLAN: { min: 4, max: 5, tip: '分值应在4-5分之间' },
@@ -148,10 +185,13 @@ export class ExampleComponent implements OnInit, OnDestroy {
         GENERAL: { min: 0, max: 5, tip: '分值应在5分以下' }
       }
     };
-    this.evaluations[index][type + 'Score'] = 0;
     this.aboutTips[index][type].tip = typeMap[type][val].tip;
     this.aboutTips[index][type].min = typeMap[type][val].min;
     this.aboutTips[index][type].max = typeMap[type][val].max;
+  }
+  changeTip(type, val, index) {
+    this.getTip(type, val, index);
+    this.evaluations[index][type + 'Score'] = 0;
   }
   getTotal(i) {
     let score1 = this.evaluations[i].taskSourceScore || 0,
